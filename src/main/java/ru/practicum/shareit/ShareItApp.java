ffd9b430448ecf2c89769e178c30d9073129b3be
gdiagilev <gdiagilev@yandex.ru>
2026-03-12 01:21:12 +0300

    Доработан файл application-test.properties

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShareItApp.class, args);
+        SpringApplication app = new SpringApplication(ShareItApp.class);
+        app.setAdditionalProfiles("test");
+        app.run(args);
     }
-
 }
